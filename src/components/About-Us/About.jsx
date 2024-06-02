32c96d5e3ea2b3eec8a5873c64567b4a2740f4d5
Saral Bajracharya <rmanbajra@gmail.com>
2024-06-02 16:58:30 +0545

    Why Choose Us section and About US modified

--- src/components/About-Us/About.jsx
+++ src/components/About-Us/About.jsx
@@ -1,3 +1,32 @@
+// import React from 'react';
+// import './About.css'; // Custom CSS for additional styling
+
+// function AboutUs() {
+//     return (
+//         <div className="container my-5 about-us-container">
+//             <div className="row">
+//                 <div className="col-12">
+//                     <div className="header">
+//                         <div className="line"></div>
+//                     </div>
+//                 </div>
+//                 <div className="col-md-6">
+//                     <img src={require('../../img/building.jpg')} alt="Building" className="img-fluid" />
+//                 </div>
+//                 <div className="col-md-6 d-flex flex-column justify-content-center">
+//                     <h2 className="font-weight-bold">ABOUT US</h2>
+//                     <p>The job market is evolving and changing quicker than ever before and so should you. At times like these, it is natural for both the job providers and job seekers to experience skill gaps.</p>
+//                     <p>At KTS, we work towards solving this very problem. We have identified skill gaps across various industries and curated courses that train, empower, and equip our learners to take on the world!</p>
+//                     <p>We provide courses that provide foundational skills, core skills, and specific skills to individuals so that they can easily fit into institutions within specific industries.</p>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default AboutUs;
+
+
 import React from 'react';
 import './About.css'; // Custom CSS for additional styling
 
@@ -6,15 +35,16 @@ function AboutUs() {
         <div className="container my-5 about-us-container">
             <div className="row">
                 <div className="col-12">
-                    <div className="header">
+                    <div className="image-line-wrapper">
+                        <h2 className="font-weight-bold">ABOUT US</h2>
                         <div className="line"></div>
                     </div>
                 </div>
                 <div className="col-md-6">
-                    <img src={require('../../img/building.jpg')} alt="Building" className="img-fluid" />
+                    <img src={require('../../img/building.jpg')} alt="Building" className="img-fluid" style={{ height: '100%' }} />
                 </div>
                 <div className="col-md-6 d-flex flex-column justify-content-center">
-                    <h2 className="font-weight-bold">ABOUT US</h2>
+
                     <p>The job market is evolving and changing quicker than ever before and so should you. At times like these, it is natural for both the job providers and job seekers to experience skill gaps.</p>
                     <p>At KTS, we work towards solving this very problem. We have identified skill gaps across various industries and curated courses that train, empower, and equip our learners to take on the world!</p>
                     <p>We provide courses that provide foundational skills, core skills, and specific skills to individuals so that they can easily fit into institutions within specific industries.</p>
--- src/components/whyChoose/whyChoose.jsx
+++ src/components/whyChoose/whyChoose.jsx
@@ -2,35 +2,46 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserGraduate, faToolbox, faGraduationCap, faUserFriends } from '@fortawesome/free-solid-svg-icons';
+import './whyChoose.css';
 
-const whyChoose = () => {
+const WhyChooseUs = () => {
     return (
-        <Container>
-            <h2 className="text-center my-5">WHY CHOOSE US</h2>
-            <p className="text-center mb-5">
-                At KTS, we offer quality and in-demand courses that equip you with sought-after skills. We improve your learning experience by focusing on competency-based learning. We provide you with the opportunity to master the skills you need to move ahead in your career.
-            </p>
-            <Row className="mb-5">
-                <Col md={3} className="text-center">
-                    <FontAwesomeIcon icon={faUserGraduate} size="3x" className="mb-3" />
-                    <h5>Organizational Training</h5>
-                </Col>
-                <Col md={3} className="text-center">
-                    <FontAwesomeIcon icon={faToolbox} size="3x" className="mb-3" />
-                    <h5>Job Placement</h5>
-                </Col>
-                <Col md={3} className="text-center">
-                    <FontAwesomeIcon icon={faGraduationCap} size="3x" className="mb-3" />
-                    <h5>Vocational Training</h5>
-                </Col>
-                <Col md={3} className="text-center">
-                    <FontAwesomeIcon icon={faUserFriends} size="3x" className="mb-3" />
-                    <h5>Industry Expert Faculty</h5>
-                </Col>
-            </Row>
-            <img src="classroom.jpg" alt="Classroom" className="img-fluid" />
-        </Container>
-    )
-}
+        <div className="whyChoose-container">
+            <Container className="my-5">
+                <div className="whyChoose-line-wrapper">
+                    <h2 className="font-weight-bold">WHY CHOOSE US</h2>
+                    <div className="line"></div>
+                </div>
+                <Row>
+                    <Col md={6} className="d-flex flex-column justify-content-center">
+                        <p>At KTS, we offer quality and in-demand courses that equip you with sought-after skills. We improve your learning experience by focusing on competency-based learning. We provide you with the opportunity to master the skills you need to move ahead in your career.</p>
+                        <p>The emphasis is on demonstrating profound understanding through application. Each student's learning experience is personalized by our trainers and teachers, who make sure that you have complete mastery of the course before graduating.</p>
+                    </Col>
+                    <Col md={6}>
+                        <img src={require('../../img/classroom.jpg')} alt="Classroom" className="img-fluid" />
+                    </Col>
+                </Row>
+                <Row className="mt-5">
+                    <Col md={6} className="text-center">
+                        <FontAwesomeIcon icon={faUserGraduate} size="1x" className="mb-3" />
+                        <h5>Organizational Training</h5>
+                    </Col>
+                    <Col md={6} className="text-center">
+                        <FontAwesomeIcon icon={faToolbox} size="1x" className="mb-3" />
+                        <h5>Job Placement</h5>
+                    </Col>
+                    <Col md={6} className="text-center">
+                        <FontAwesomeIcon icon={faGraduationCap} size="1x" className="mb-3" />
+                        <h5>Vocational Training</h5>
+                    </Col>
+                    <Col md={6} className="text-center">
+                        <FontAwesomeIcon icon={faUserFriends} size="1x" className="mb-3" />
+                        <h5>Industry Expert Faculty</h5>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
 
-export default whyChoose
+export default WhyChooseUs;
