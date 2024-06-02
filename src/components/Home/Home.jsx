d60a60b2e07f8377af9a806bd36c54da58504e8a
Saral Bajracharya <rmanbajra@gmail.com>
2024-06-02 15:42:06 +0545

    Video section added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,27 +1,14 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import About from '../About-Us/About'
-// import Video from '../../vid/Homepage.mp4'
+import Video from '../Video/Video'
 
 const Home = () => {
     return (
         <>
             <Navbar />
+            <Video />
             <About />
-            <div className="home">
-                {/* Video Section */}
-                {/* <div className="video-container">
-                    <video className="video" controls>
-                        <source src={Video} type="video/mp4" />
-                        Your browser does not support the video tag.
-                    </video>
-                </div> */}
-
-                {/* Other content */}
-                <div className="other-content">
-                    {/* Add other content here */}
-                </div>
-            </div>
         </>
     )
 }
---
+++ src/components/Video/Video.jsx
@@ -0,0 +1,51 @@
+import React, { useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPause, faVolumeMute, faVolumeUp } from '@fortawesome/free-solid-svg-icons';
+import Video from '../../vid/Homepage.mp4';
+import './Video.css';
+
+const Videos = () => {
+    const videoRef = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(true);
+    const [isMuted, setIsMuted] = useState(true);
+
+    const handlePlayPause = () => {
+        if (videoRef.current.paused) {
+            videoRef.current.play();
+            setIsPlaying(true);
+        } else {
+            videoRef.current.pause();
+            setIsPlaying(false);
+        }
+    };
+
+    const handleMuteUnmute = () => {
+        videoRef.current.muted = !videoRef.current.muted;
+        setIsMuted(videoRef.current.muted);
+    };
+
+    return (
+        <div className="video-container">
+            <video
+                className="video"
+                ref={videoRef}
+                autoPlay
+                muted
+                onClick={handlePlayPause}
+            >
+                <source src={Video} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            <div className="controls">
+                <button className="button" onClick={handlePlayPause}>
+                    <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} />
+                </button>
+                <button className="button" onClick={handleMuteUnmute}>
+                    <FontAwesomeIcon icon={isMuted ? faVolumeMute : faVolumeUp} />
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Videos;
