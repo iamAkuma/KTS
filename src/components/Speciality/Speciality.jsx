029c29268593147f5e8309da0a7cf150b28d77ac
Saral Bajracharya <rmanbajra@gmail.com>
2024-06-03 15:47:21 +0545

    Quote Section changes

--- src/components/Speciality/Speciality.jsx
+++ src/components/Speciality/Speciality.jsx
@@ -7,25 +7,25 @@ const OurSpeciality = () => {
             <div className="row align-items-center">
                 <div className="col-md-6">
                     <h1 className="text-center mb-5">OUR SPECIALITY</h1>
-                    <div className="d-flex align-items-center mb-4">
+                    <div className="d-flex align-items-center mb-2">
                         <h3>Learn Skills</h3>
                     </div>
                     <p>
                         Acquire skills that make you job-ready across industries, whether you are an individual, an organization, or a government body - get the training you need for yourself and your workforce to succeed efficiently.
                     </p>
-                    <div className="d-flex align-items-center mb-4">
+                    <div className="d-flex align-items-center mb-2">
                         <h3>Industry Experts</h3>
                     </div>
                     <p>
                         Our teachers are individuals with multitude of experience in the said profession. Our programmes will ensure that this skills training contributes to the end goal of employment.
                     </p>
-                    <div className="d-flex align-items-center mb-4">
+                    <div className="d-flex align-items-center mb-2">
                         <h3>Thriving Outcomes</h3>
                     </div>
                     <p>
                         We aim to create skilled individuals and get exponential business outcomes. In just 3-6 months, you'll have the abilities you need to advance your career.
                     </p>
-                    <div className="d-flex align-items-center mb-4">
+                    <div className="d-flex align-items-center mb-2">
                         <h3>Earn Certificates</h3>
                     </div>
                     <p>
