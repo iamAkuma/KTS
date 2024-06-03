a1791dae704eb0038b7353b715568e6a2b3f9d95
Saral Bajracharya <rmanbajra@gmail.com>
2024-06-03 14:51:12 +0545

    quote section changes

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Quote.css';
 
 const Quote = () => {
@@ -6,13 +8,13 @@ const Quote = () => {
         <div className="quote-section">
             <div className="quote-container">
                 <div className="quote-icon-left">
-                    <span>&ldquo;</span>
+                    <FontAwesomeIcon icon={faQuoteLeft} className="quote-icon" />
                 </div>
                 <p className="quote-text">
-                    We strive to provide skills advancement course to youth that prepares them for employability in both national and global labor markets.
+                    We strive to provide skills advancement courses to youth that prepare them for employability in both national and global labor markets.
                 </p>
                 <div className="quote-icon-right">
-                    <span>&rdquo;</span>
+                    <FontAwesomeIcon icon={faQuoteRight} className="quote-icon" />
                 </div>
             </div>
         </div>
