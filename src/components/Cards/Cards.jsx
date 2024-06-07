31e0d2ea1c0d1be0c471ca0db40c9cf7e53ee407
Saral Bajracharya <rmanbajra@gmail.com>
2024-06-07 16:13:26 +0545

    Course Card added Need to make changes with the image

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,8 @@
 // src/components/CaregiverCard.js
 import React from 'react';
 import './CaregiverCard.css'; // Import custom CSS for the component
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faHandHoldingHeart, faHandsHelping, faCocktail } from '@fortawesome/free-solid-svg-icons';
 
 const CaregiverCard = () => {
     return (
@@ -11,23 +13,50 @@ const CaregiverCard = () => {
                     <div className="card-header bg-primary text-white">
                         <h3 className='card-head'>Caregiver/Aged Care</h3>
                         <div className="icon-container">
-                            <i className="fa fa-user-md" aria-hidden="true"></i>
+                            <FontAwesomeIcon icon={faHandHoldingHeart} />
                         </div>
                     </div>
                     <div className="image-container">
-                        <img src={require('../../img/classroom.jpg')} className="card-img-top img-fluid" alt="Caregivers" />
+                        <img src={require('../../img/Caregiver.jpg')} className="card-img-top img-fluid" alt="Caregivers" />
                         <button className="btn btn-primary btn-lg">Learn More</button>
                     </div>
                 </div>
                 <div className="card caregiver-card">
                     <div className="card-header bg-primary text-white">
-                        <h3 className='card-head'>Caregiver/Aged Care</h3>
+                        <h3 className='card-head'>Barista</h3>
                         <div className="icon-container">
-                            <i className="fa fa-user-md" aria-hidden="true"></i>
+                            <FontAwesomeIcon icon={faCoffee} />
                         </div>
                     </div>
                     <div className="image-container">
-                        <img src={require('../../img/classroom.jpg')} className="card-img-top img-fluid" alt="Caregivers" />
+                        <img src={require('../../img/Barista.jpg')} className="card-img-top img-fluid" alt="Barista" />
+                        <button className="btn btn-primary btn-lg">Learn More</button>
+                    </div>
+                </div>
+            </div>
+            {/* Add the second row here */}
+            <div className="cards-wrapper">
+                <div className="card caregiver-card">
+                    <div className="card-header bg-primary text-white">
+                        <h3 className='card-head'>Certificate III in Individual Support</h3>
+                        <div className="icon-container">
+                            <FontAwesomeIcon icon={faHandsHelping} />
+                        </div>
+                    </div>
+                    <div className="image-container">
+                        <img src={require('../../img/Cert.jpg')} className="card-img-top img-fluid" alt="Caregivers" />
+                        <button className="btn btn-primary btn-lg">Learn More</button>
+                    </div>
+                </div>
+                <div className="card caregiver-card">
+                    <div className="card-header bg-primary text-white">
+                        <h3 className='card-head'>Bartender</h3>
+                        <div className="icon-container">
+                            <FontAwesomeIcon icon={faCocktail} />
+                        </div>
+                    </div>
+                    <div className="image-container">
+                        <img src={require('../../img/Bartender.jpg')} className="card-img-top img-fluid" alt="Bartender" />
                         <button className="btn btn-primary btn-lg">Learn More</button>
                     </div>
                 </div>
