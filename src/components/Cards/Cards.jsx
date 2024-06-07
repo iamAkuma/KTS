d6432aac173a821d957173bd3d027e0840ea4618
Saral Bajracharya <rmanbajra@gmail.com>
2024-06-07 15:33:45 +0545

    Course Cards added

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,23 +1,38 @@
+// src/components/CaregiverCard.js
 import React from 'react';
 import './CaregiverCard.css'; // Import custom CSS for the component
 
 const CaregiverCard = () => {
     return (
-        <>
+        <div className="caregiver-cards-container">
             <h1 className='course-header'>Current Courses</h1> {/* Use h1 and center styling in JSX */}
-            <div className="card caregiver-card">
-                <div className="card-header bg-primary text-white">
-                    <h3 className='card-head'>Caregiver/Aged Care</h3>
-                    <div className="icon-container">
-                        <i className="fa fa-user-md" aria-hidden="true"></i>
+            <div className="cards-wrapper">
+                <div className="card caregiver-card">
+                    <div className="card-header bg-primary text-white">
+                        <h3 className='card-head'>Caregiver/Aged Care</h3>
+                        <div className="icon-container">
+                            <i className="fa fa-user-md" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <div className="image-container">
+                        <img src={require('../../img/classroom.jpg')} className="card-img-top img-fluid" alt="Caregivers" />
+                        <button className="btn btn-primary btn-lg">Learn More</button>
                     </div>
                 </div>
-                <div className="image-container">
-                    <img src={require('../../img/classroom.jpg')} className="card-img-top img-fluid" alt="Caregivers" />
-                    <button className="btn btn-primary btn-lg">Learn More</button>
+                <div className="card caregiver-card">
+                    <div className="card-header bg-primary text-white">
+                        <h3 className='card-head'>Caregiver/Aged Care</h3>
+                        <div className="icon-container">
+                            <i className="fa fa-user-md" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <div className="image-container">
+                        <img src={require('../../img/classroom.jpg')} className="card-img-top img-fluid" alt="Caregivers" />
+                        <button className="btn btn-primary btn-lg">Learn More</button>
+                    </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
