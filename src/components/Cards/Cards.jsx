e9e07cd3d6cfbd9c3f5b3cbe5c0b53d0e3a2ede4
Saral Bajracharya <rmanbajra@gmail.com>
2024-06-08 15:28:05 +0545

    Course Card section improvement

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,62 +1,62 @@
 // src/components/CaregiverCard.js
 import React from 'react';
-import './Cards.css'; // Import custom CSS for the component
+import './Cards.css'; // Update the CSS file name
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee, faHandHoldingHeart, faHandsHelping, faCocktail } from '@fortawesome/free-solid-svg-icons';
 
-const CaregiverCard = () => {
+const Tabs = () => {
     return (
-        <div className="caregiver-cards-container">
+        <div className="caregiver-tabs-container">
             <h1 className='course-header'>Current Courses</h1> {/* Use h1 and center styling in JSX */}
-            <div className="cards-wrapper">
-                <div className="card caregiver-card">
-                    <div className="card-header bg-primary text-white">
-                        <h3 className='card-head'>Caregiver/Aged Care</h3>
+            <div className="tabs-wrapper">
+                <div className="tab caregiver-tab">
+                    <div className="tab-header text-white">
+                        <h3 className='tab-head'>Caregiver/Aged Care</h3>
                         <div className="icon-container">
                             <FontAwesomeIcon icon={faHandHoldingHeart} />
                         </div>
                     </div>
                     <div className="image-container">
-                        <img src={require('../../img/Caregiver.jpg')} className="card-img-top img-fluid" alt="Caregivers" />
+                        <img src={require('../../img/Caregiver.jpg')} className="tab-img-top img-fluid" alt="Caregivers" />
                         <button className="btn btn-primary btn-lg">Learn More</button>
                     </div>
                 </div>
-                <div className="card caregiver-card">
-                    <div className="card-header bg-primary text-white">
-                        <h3 className='card-head'>Barista</h3>
+                <div className="tab caregiver-tab">
+                    <div className="tab-header text-white">
+                        <h3 className='tab-head'>Barista</h3>
                         <div className="icon-container">
                             <FontAwesomeIcon icon={faCoffee} />
                         </div>
                     </div>
                     <div className="image-container">
-                        <img src={require('../../img/Barista.jpg')} className="card-img-top img-fluid" alt="Barista" />
+                        <img src={require('../../img/Barista.jpg')} className="tab-img-top img-fluid" alt="Barista" />
                         <button className="btn btn-primary btn-lg">Learn More</button>
                     </div>
                 </div>
             </div>
             {/* Add the second row here */}
-            <div className="cards-wrapper">
-                <div className="card caregiver-card">
-                    <div className="card-header bg-primary text-white">
-                        <h3 className='card-head'>Certificate III in Individual Support</h3>
+            <div className="tabs-wrapper">
+                <div className="tab caregiver-tab">
+                    <div className="tab-header  text-white">
+                        <h3 className='tab-head'>Certificate III in Individual Support</h3>
                         <div className="icon-container">
                             <FontAwesomeIcon icon={faHandsHelping} />
                         </div>
                     </div>
                     <div className="image-container">
-                        <img src={require('../../img/Cert.jpg')} className="card-img-top img-fluid" alt="Caregivers" />
+                        <img src={require('../../img/Cert.jpg')} className="tab-img-top img-fluid" alt="Caregivers" />
                         <button className="btn btn-primary btn-lg">Learn More</button>
                     </div>
                 </div>
-                <div className="card caregiver-card">
-                    <div className="card-header bg-primary text-white">
-                        <h3 className='card-head'>Bartender</h3>
+                <div className="tab caregiver-tab">
+                    <div className="tab-header text-white">
+                        <h3 className='tab-head'>Bartender</h3>
                         <div className="icon-container">
                             <FontAwesomeIcon icon={faCocktail} />
                         </div>
                     </div>
                     <div className="image-container">
-                        <img src={require('../../img/Bartender.jpg')} className="card-img-top img-fluid" alt="Bartender" />
+                        <img src={require('../../img/Bartender.jpg')} className="tab-img-top img-fluid" alt="Bartender" />
                         <button className="btn btn-primary btn-lg">Learn More</button>
                     </div>
                 </div>
@@ -65,4 +65,4 @@ const CaregiverCard = () => {
     );
 };
 
-export default CaregiverCard;
+export default Tabs;
