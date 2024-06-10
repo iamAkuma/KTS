f9e1e2559568c16b9cb1588412631c08a1deabed
Saral Bajracharya <rmanbajra@gmail.com>
2024-06-10 13:58:57 +0545

    Design added to the content

--- src/components/Scroll/Scroll.jsx
+++ src/components/Scroll/Scroll.jsx
@@ -5,7 +5,7 @@ import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
 const events = [
     { title: 'Holistic Approach', description: 'We take a comprehensive approach to teaching where our educators address the emotional, social, ethical, and academic needs of students in an enabling learning environment.' },
-    { title: 'Result Oriented', description: 'We provide relevant training which is contextualized and purposely tailored to the learner or community needs and goals. Our courses ensure skills, competency, and employability.' },
+    { title: 'Result Oriented', description: 'We provide relevant training which is contextualized and purposely tailored to the learner or community’s needs and goals. Our courses ensure skills, competency, and employability.' },
     { title: 'Inclusivity', description: 'We provide real learning opportunities for groups who have traditionally been excluded. KTS provides an environment where all students feel an extended sense of belonging.' },
     { title: 'Employer Engaged', description: 'We actively involve potential employers who mentor and directly engage our students through hands-on activities and mentorship in real work environments.' },
     { title: 'Market Oriented', description: 'Our courses are based on and driven by real-time and future market needs. Every season we introduce courses that are market-, jobs-, and industry-oriented.' }
