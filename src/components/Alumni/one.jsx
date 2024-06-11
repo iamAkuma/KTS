85ef79c313a77293f61bae93f062410aa7e10e15
Saral Bajracharya <rmanbajra@gmail.com>
2024-06-11 13:34:12 +0545

    Alumni Section done

--- src/components/Alumni/one.jsx
+++ src/components/Alumni/one.jsx
@@ -1,74 +1,74 @@
-// import React from 'react';
-// import Navbar from '../Navbar/Navbar';
-// import Footer from '../Footer/Footer';
-// import './one.css';
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
+import './one.css';
 
-// const Two = () => {
-//   // Array of image details
-//   const pictures = [
-//     {
-//       src: require('../../img/Amita-Raut.jpg'),
-//       alt: 'Amita Raut',
-//       name: 'Amita Raut'
-//     },
-//     {
-//       src: require('../../img/Amrita-Sunuwar.jpg'),
-//       alt: 'Amrita Sunuwar',
-//       name: 'Amrita Sunuwar'
-//     },
-//     {
-//       src: require('../../img/Bikram-Puri-903x1024.jpg'),
-//       alt: 'Bikram Puri',
-//       name: 'Bikram Puri'
-//     },
-//     {
-//       src: require('../../img/Durga-Manandhar-1024x995.jpg'),
-//       alt: 'Durga Manandhar',
-//       name: 'Durga Manandhar'
-//     },
-//     {
-//       src: require('../../img/Reyzina-Pujari.jpeg'),
-//       alt: 'Reyzina Pujari',
-//       name: 'Reyzina Pujari'
-//     },
-//     {
-//       src: require('../../img/Ritu-Sinha-842x1024.jpeg'),
-//       alt: 'Ritu Kumari Sinha',
-//       name: 'Ritu Kumari Sinha'
-//     },
-//     {
-//       src: require('../../img/Saraswati-Gurung-874x1024.jpg'),
-//       alt: 'Saraswati Gurung',
-//       name: 'Saraswati Gurung'
-//     },
-//     {
-//       src: require('../../img/Sujana-Adhikari.jpeg'),
-//       alt: 'Sujana Adhikari',
-//       name: 'Sujana Adhikari'
-//     }
-//   ];
+const Two = () => {
+    // Array of image details
+    const pictures = [
+        {
+            src: require('../../img/Amita-Raut.jpg'),
+            alt: 'Amita Raut',
+            name: 'Amita Raut'
+        },
+        {
+            src: require('../../img/Amrita-Sunuwar.jpg'),
+            alt: 'Amrita Sunuwar',
+            name: 'Amrita Sunuwar'
+        },
+        {
+            src: require('../../img/Bikram-Puri-903x1024.jpg'),
+            alt: 'Bikram Puri',
+            name: 'Bikram Puri'
+        },
+        {
+            src: require('../../img/Durga-Manandhar-1024x995.jpg'),
+            alt: 'Durga Manandhar',
+            name: 'Durga Manandhar'
+        },
+        {
+            src: require('../../img/Reyzina-Pujari.jpeg'),
+            alt: 'Reyzina Pujari',
+            name: 'Reyzina Pujari'
+        },
+        {
+            src: require('../../img/Ritu-Sinha-842x1024.jpeg'),
+            alt: 'Ritu Kumari Sinha',
+            name: 'Ritu Kumari Sinha'
+        },
+        {
+            src: require('../../img/Saraswati-Gurung-874x1024.jpg'),
+            alt: 'Saraswati Gurung',
+            name: 'Saraswati Gurung'
+        },
+        {
+            src: require('../../img/Sujana-Adhikari.jpeg'),
+            alt: 'Sujana Adhikari',
+            name: 'Sujana Adhikari'
+        }
+    ];
 
-//   return (
-//     <>
-//       <Navbar />
-//       <div>
-//         <h1 className='one-header'>
-//           2079/80
-//         </h1>
-//         {/* Image section */}
-//         <div className="pic-container">
-//           {/* Map over the images array */}
-//           {pictures.map((picture, index) => (
-//             <div className="pic-wrapper" key={index}>
-//               <img src={picture.src} alt={picture.alt} className="pic" />
-//               <p className="name">{picture.name}</p>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//       <Footer />
-//     </>
-//   );
-// };
+    return (
+        <>
+            <Navbar />
+            <div>
+                <h1 className='one-header'>
+                    2079/80
+                </h1>
+                {/* Image section */}
+                <div className="pic-container">
+                    {/* Map over the images array */}
+                    {pictures.map((picture, index) => (
+                        <div className="pic-wrapper" key={index}>
+                            <img src={picture.src} alt={picture.alt} className="pic" />
+                            <p className="name">{picture.name}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
+};
 
-// export default Two;
+export default Two;
