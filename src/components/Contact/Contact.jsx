42b442230066ad2646059ed0009b609c691eab4b
Saral Bajracharya <rmanbajra@gmail.com>
2024-06-11 14:44:56 +0545

    Contact Section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
+import './Contact.css'
 
 const Contact = () => {
     return (
         <>
             <Navbar />
             <div>
-                <h1 className='text-center'>Contact</h1>
+                <h1 className='text-header'>Contact</h1>
             </div>
             <Footer />
         </>
