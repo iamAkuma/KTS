c0936a673c80f051ee2c9a789b85fa8606456573
Saral Bajracharya <rmanbajra@gmail.com>
2024-06-13 16:16:29 +0545

    Adding loading in Blogs

--- src/components/Blog/Blogs.jsx
+++ src/components/Blog/Blogs.jsx
@@ -1,23 +1,38 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
-import Footer from '../Footer/Footer'
+import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
+import LoadingScreen from '../LoadingScreen/LoadingScreen'; // Adjust path as necessary
+import './Blogs.css'; // Adjust path as necessary
 
 const Blogs = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 3000); // Adjust time in milliseconds (e.g., 3000 for 3 seconds)
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <>
             <Navbar />
-            <Helmet>
-                <title>
-                    Blogs - Kathmandu Technical School
-                </title>
-            </Helmet>
-            <div>
-                <h1 className='text-center'>KTS Blogs</h1>
-            </div>
-            <Footer />
-        </>
-    )
-}
+            <LoadingScreen isLoading={isLoading} />
+            <div className={`blogs-container ${isLoading ? 'hidden' : 'visible'}`}>
+                <Helmet>
+                    <title>Blogs - Kathmandu Technical School</title>
+                </Helmet>
 
-export default Blogs
+                <div>
+                    <h1 className='text-center'>KTS Blogs</h1>
+                    {/* Add your blog content here */}
+                </div>
+                <Footer />
+            </div>
+        </>
+    );
+};
+
+export default Blogs;
