c71704b6b0d6bd9bc0863b5a21ec971c0709b2f4
Saral Bajracharya <rmanbajra@gmail.com>
2024-06-18 14:06:33 +0545

    Courses added

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -8,6 +8,12 @@ import './Courses.css'; // Adjust path as necessary
 
 import image1 from '../../img/Caregiver.jpg';  // Update the paths to your images
 import image2 from '../../img/Cert.jpg';
+import image3 from '../../img/barista1.jpg';
+import image4 from '../../img/cook.jpg';
+import image5 from '../../img/bartenderr.jpg';
+import image6 from '../../img/waiter.jpg';
+import image7 from '../../img/house.jpg';
+
 
 const Courses = () => {
     const [isLoading, setIsLoading] = useState(true);
@@ -30,6 +36,31 @@ const Courses = () => {
             image: image2,
             title: 'Certificate III in Individual Support (Ageing and Disability)',
             description: 'This course boosts skills in providing personalized care to the elderly in community and residential settings, addressing aging and disability-related needs with high-quality support and compassion.'
+        },
+        {
+            image: image3,// Replace with actual paths
+            title: 'Barista',
+            description: 'Learn the art of making perfect coffee.'
+        },
+        {
+            image: image4, // Replace with actual paths
+            title: 'Commercial Cook / Baker',
+            description: 'Become a skilled commercial cook or baker.'
+        },
+        {
+            image: image5, // Replace with actual paths
+            title: 'Bartender',
+            description: 'Master the skills of bartending.'
+        },
+        {
+            image: image6, // Replace with actual paths
+            title: 'Waiter / Waitress',
+            description: 'Train to be an efficient waiter or waitress.'
+        },
+        {
+            image: image7, // Replace with actual paths
+            title: 'Housekeeping Cleaner',
+            description: 'Learn professional housekeeping skills.'
         }
     ];
 
@@ -43,10 +74,8 @@ const Courses = () => {
                 </Helmet>
                 <div className='courses-page'> {/* Add this class */}
                     <h1 className='course-header'>Our Courses</h1>
+                    <h3 className='course-sub'>Caregiver / Aged Care</h3>
                     <div className="row">
-                        <div className="card-header">
-                            Caregiver / Aged Care Courses
-                        </div>
                         {cardsData.map((card, index) => (
                             <CardComponent
                                 key={index}
