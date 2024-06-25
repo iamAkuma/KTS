7f256388493e7cabd1e0169ba8adf254d3b7cca3
Saral Bajracharya <rmanbajra@gmail.com>
2024-06-25 17:22:44 +0545

    Added Loading Screen in Alumni Page

--- src/components/Alumni/Alumni.jsx
+++ src/components/Alumni/Alumni.jsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
+import LoadingScreen from '../LoadingScreen/LoadingScreen'; // Adjust path as necessary
 import './Alumni.css';
 
 const Alumni = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 1500); // Adjust time in milliseconds (e.g., 3000 for 3 seconds)
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <>
             <Navbar />
-            <div className='alumni-section'>
-                <h1 className='alumni-header'>ALUMNI</h1>
-                <div className='year-section'>
-                    <ul>
-                        <Link className='seventy' to="/2079-80">
-                            <li>2079/80</li>
-                        </Link>
-                        <Link className='eighty' to="/2080-81">
-                            <li>2080/81</li>
-                        </Link>
-                        {/* Add more years as needed */}
-                    </ul>
+            <LoadingScreen isLoading={isLoading} />
+            <div className={`alumni-container ${isLoading ? 'hidden' : 'visible'}`}>
+                <Helmet>
+                    <title>Alumni - Kathmandu Technical School</title>
+                </Helmet>
+                <div className='alumni-section'>
+                    <h1 className='alumni-header'>ALUMNI</h1>
+                    <div className='year-section'>
+                        <ul>
+                            <Link className='seventy' to="/2079-80">
+                                <li>2079/80</li>
+                            </Link>
+                            <Link className='eighty' to="/2080-81">
+                                <li>2080/81</li>
+                            </Link>
+                            {/* Add more years as needed */}
+                        </ul>
+                    </div>
                 </div>
+                <Footer />
             </div>
         </>
     );
