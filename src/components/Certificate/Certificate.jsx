6d90b3701e25a78f3d99ae9e028cc3162c6b544b
Saral Bajracharya <rmanbajra@gmail.com>
2024-06-30 15:35:52 +0545

    Certificate-III page mostly finished

--- src/components/Certificate/Certificate.jsx
+++ src/components/Certificate/Certificate.jsx
@@ -3,6 +3,7 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 import './Certificate.css';
 import Navbar from '../Navbar/Navbar';
 import pdfFile from '../../img/Certificate-III-in-Individual-Support-Ageing-and-Disability_112023_Inno-1.pdf'
+import Footer from '../Footer/Footer'
 
 function Certificate() {
     return (
@@ -23,8 +24,19 @@ function Certificate() {
                         </div>
                         <Button href={pdfFile} variant="primary" size="lg">Download Course Details</Button>
                     </Col>
+                    <Col>
+                        <div>
+                            <p className='certificate'>
+                                This <b>Australian-certified program</b>,
+                                in collaboration with  <b>Kathmandu Technical School</b>
+                                and in partnership with <b>Crown Institute of Business and Technology</b> , is tailored to enhance your skills in providing physical and emotional support to the elderly. It mirrors the roles of care workers in community and residential settings, where personalized plans are crafted to assist individuals dealing with aging or disability-related issues. Through this course, you’ll apply technical, theoretical,
+                                and procedural knowledge to deliver exemplary support and care to vulnerable seniors.
+                            </p>
+                        </div>
+                    </Col>
                 </Row>
             </Container>
+            <Footer />
         </>
     );
 }
