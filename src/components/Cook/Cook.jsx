37d89416c36b4651d84fcb305831b673dd872feb
Saral Bajracharya <rmanbajra@gmail.com>
2024-06-30 16:20:10 +0545

    Commerical Cook page added

--- src/components/Cook/Cook.jsx
+++ src/components/Cook/Cook.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import './Cook.css'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer'
 
-const Cook = () => {
+function Cook() {
     return (
-        <div>Cook</div>
-    )
+        <>
+            <Navbar />
+            <Container fluid className="unique-caregiver-course-container">
+                <Row className="align-items-center">
+                    <Col md={6} className="unique-caregiver-course-image">
+                        <img src={require('../../img/cook.jpg')} alt="Course" className="img-fluid" />
+                    </Col>
+                    <Col md={6} className="unique-caregiver-course-details">
+                        <h1>Commercial Cook / Baker <br></br>
+                        </h1>
+                        <div className="underline"></div>
+                        <div>
+                            <p className="unique-caregiver-course-description">
+                                The Commercial Chef and Baker program is a culinary training
+                                course designed for aspiring professionals in the food industry.</p>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                            <p className='certificate'>
+                                The Commercial Chef and Baker program offers a thorough training experience for those aspiring to succeed in the culinary world. This comprehensive course blends theoretical learning with hands-on practice, covering everything from ingredient selection to cooking and baking techniques, menu planning, kitchen safety, and customer service. Led by seasoned instructors, students have the chance to unleash their creativity and perfect their skills, preparing them for rewarding careers in commercial kitchens and bakeries.
+                            </p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <Footer />
+        </>
+    );
 }
 
-export default Cook
+export default Cook;
