c2471dfa9902933f6c7f34d5fe130d43585e8ebf
Saral Bajracharya <rmanbajra@gmail.com>
2024-06-30 16:17:21 +0545

    Barista page made

--- src/components/Barista/Barista.jsx
+++ src/components/Barista/Barista.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import './Bartender.css';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer'
 
@@ -11,31 +10,22 @@ function Bartender() {
             <Container fluid className="unique-caregiver-course-container">
                 <Row className="align-items-center">
                     <Col md={6} className="unique-caregiver-course-image">
-                        <img src={require('../../img/Bartender.jpg')} alt="Course" className="img-fluid" />
+                        <img src={require('../../img/Barista.jpg')} alt="Course" className="img-fluid" />
                     </Col>
                     <Col md={6} className="unique-caregiver-course-details">
-                        <h1>Barista</h1>
+                        <h1>Barista <br></br>
+                            Course</h1>
                         <div className="underline"></div>
                         <div>
                             <p className="unique-caregiver-course-description">
-                                The Bartender Class trains in mixology,
-                                beverage service, and bar management. Led by experienced instructors,
-                                it prepares individuals for success in bartending.
-                            </p>
+                                The Barista Course is a hospitality management course that consists of theory and practical based approach to coffee. From the history of coffee to fancy drinks, our course covers it all!                            </p>
                         </div>
                     </Col>
                     <Col>
                         <div>
                             <p className='certificate'>
-                                The Bartender Class is an immersive training
-                                program tailored to individuals seeking mastery in the art of mixology,
-                                beverage service, and bar management. With a perfect blend of hands-on practice
-                                and comprehensive theoretical instruction, students delve into the intricate
-                                world of crafting cocktails, mastering liquor knowledge, honing customer
-                                service skills, and understanding the nuances of effective bar management.
-                                Guided by seasoned professionals, participants are equipped not only with
-                                technical expertise but also with the confidence and
-                                finesse required to thrive in the dynamic and fast-paced environment of bartending.                            </p>
+                                Our Barista Class offers a comprehensive exploration into the intricate world of coffee craftsmanship. Led by seasoned instructors deeply passionate about coffee, the program integrates theory with practical learning. Participants delve into the rich history of coffee, mastering both traditional and fancy recipes. It’s a journey designed for those eager to immerse themselves in the essence of coffee culture, fostering a deeper understanding and appreciation for the craft.
+                            </p>
                         </div>
                     </Col>
                 </Row>
