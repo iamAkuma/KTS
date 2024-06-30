374b8c16f72d9eebcab38268b474dec6113935e1
Saral Bajracharya <rmanbajra@gmail.com>
2024-06-30 14:17:23 +0545

    Making Cerfiticate-III course page

--- src/components/Certificate/Certificate.jsx
+++ src/components/Certificate/Certificate.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import './Certificate.css'
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import './Certificate.css';
+import Navbar from '../Navbar/Navbar'
 
-const Certificate = () => {
+function App() {
     return (
-        <div>Certificate</div>
-    )
+        <>
+            <Navbar />
+            <Container fluid className="unique-course-container">
+                <Row className="align-items-center">
+                    <Col md={6} className="unique-course-image">
+                        <img src={require('../../img/Cert3.jpeg')} alt="Course" className="img-fluid" />
+                    </Col>
+                    <Col md={6} className="unique-course-details">
+                        <h1>Certificate III in Individual Support (Ageing and Disability)</h1>
+                        <p className="unique-course-description">
+                            This course enhances your abilities in offering both physical and emotional aid to the elderly, focusing on care provision in community and residential environments.
+                        </p>
+                        <Button href="#" variant="primary" size="lg">Download Course Details</Button>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
 }
 
-export default Certificate
+export default App;
