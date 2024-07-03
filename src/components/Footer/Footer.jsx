e211171b40084c93f5fc989bd51af6ad42e644ba
Saral Bajracharya <rmanbajra@gmail.com>
2024-07-03 14:30:46 +0545

    Footer made resposive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -34,19 +34,19 @@ const Footer = () => {
                         <div className="contact-details">
                             <p className="fs-5">
                                 <div className="phone" style={{ textDecoration: 'none', color: 'black' }}>
-                                    <i className="fas fa-phone-alt me-3" style={{ textDecoration: 'none', color: '#2b3991' , fontSize:'smaller'}}></i>
+                                    <i className="fas fa-phone-alt me-3" style={{ textDecoration: 'none', color: '#2b3991', fontSize: 'smaller' }}></i>
                                     +977 01 5912297
                                 </div>
                             </p>
                             <p className="fs-5">
                                 <i className="fas fa-envelope me-3" style={{ textDecoration: 'none', color: '#2b3991' }}></i>
-                                <a href="mailto:info@ktsnepal.com" target="_blank" style={{ textDecoration: 'none', color: 'black', fontSize:'smaller' }}>
+                                <a href="mailto:info@ktsnepal.com" target="_blank" style={{ textDecoration: 'none', color: 'black', fontSize: 'smaller' }}>
                                     info@ktsnepal.com
                                 </a>
                             </p>
                             <p className="fs-5">
-                                <i className="fas fa-map-marker-alt me-3" style={{ textDecoration: 'none', color: '#2b3991' }}></i>
-                                <a href="https://maps.app.goo.gl/rM3pUwdj9aKmL9cm9" target="_blank" style={{ textDecoration: 'none', color: 'black', fontSize:'smaller' }}>
+                                <i className="fas fa-map-marker-alt me-3 unique-location" style={{ textDecoration: 'none', color: '#2b3991' }}></i>
+                                <a href="https://maps.app.goo.gl/rM3pUwdj9aKmL9cm9" target="_blank" style={{ textDecoration: 'none', color: 'black', fontSize: 'smaller' }}>
                                     Sukedhara, Kathmandu, Nepal
                                 </a>
                             </p>
@@ -54,17 +54,17 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="unique-navbar">
-                <nav className="d-flex justify-content-center mb-4">
-                    <Link className="navi-link mx-3 footer-navi-link" to="/">Home</Link>
-                    <Link className="navi-link mx-3 footer-navi-link" to="/alumni">Alumni</Link>
-                    <Link className="navi-link mx-3 footer-navi-link" to="/courses">Courses</Link>
-                    <Link className="navi-link mx-3 footer-navi-link" to="/blog">Blog</Link>
-                    <Link className="navi-link mx-3 footer-navi-link" to="/contact">Contact</Link>
-                </nav>
+                    <nav className="d-flex justify-content-center mb-4">
+                        <Link className="navi-link mx-3 footer-navi-link" to="/">Home</Link>
+                        <Link className="navi-link mx-3 footer-navi-link" to="/alumni">Alumni</Link>
+                        <Link className="navi-link mx-3 footer-navi-link" to="/courses">Courses</Link>
+                        <Link className="navi-link mx-3 footer-navi-link" to="/blog">Blog</Link>
+                        <Link className="navi-link mx-3 footer-navi-link" to="/contact">Contact</Link>
+                    </nav>
                 </div>
                 <div className="unique-copyright">
                     <p>Copyright © {new Date().getFullYear()} <b>
-                        <Link to="/" className='unique-name'>Kathmandu Technical School</Link>
+                        <Link to="/home" className='unique-name'>Kathmandu Technical School</Link>
                     </b></p>
                 </div>
             </div>
