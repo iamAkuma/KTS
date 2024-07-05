a7a675a574c394f1504d12668a91f7f9284246f6
Saral Bajracharya <rmanbajra@gmail.com>
2024-07-05 13:39:58 +0545

    News page changes

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,35 +1,26 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
-import './News.css'
-import { Row, Col } from 'react-bootstrap';
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import './News.css';
+import image1 from '../../img/news-1.png';
+import image2 from '../../img/news-2.png';
 
 const News = () => {
     return (
         <>
             <Navbar />
             <h1 className='news-heading'>News / Articles</h1>
-            <Row>
-                <Col>
-                    <div className='news-first-row'>
-                        <img src={require('../../img/Cert.jpg')} alt="" className='news-img' />
-                        <h2 className='news-title'>आईएमई समुहले काठमाडौ टेक्निकल स्कुल खोल्यो</h2>
-                    </div>
-                </Col>
-
-                <Col>
-                    <div className='news-first-row'>
-                        <img src={require('../../img/Cert.jpg')} alt="" className='news-img' />
-                        <h2 className='news-title'>आईएमई समुहले काठमाडौ टेक्निकल स्कुल खोल्यो</h2>
-                    </div>
-                </Col>
-
-            </Row>
-
-
-
-
+            <div className='news-container'>
+                <div className='news-item'>
+                    <img src={image1} alt="News 1" id='news-img-one' />
+                    <h2 className='news-title'>आईएमई समुहले काठमाडौ टेक्निकल स्कुल खोल्यो</h2>
+                </div>
+                <div className='news-item'>
+                    <img src={image2} alt="News 2" id='news-img-two' />
+                    <h2 className='news-title'>टेक्निकल स्कुल चलाउँदै आईएमई समुह</h2>
+                </div>
+            </div>
         </>
-    )
+    );
 }
 
-export default News
+export default News;
