69647c0fab59802436daca614f9bb5cf7fe62447
Saral Bajracharya <rmanbajra@gmail.com>
2024-07-16 14:03:05 +0545

    Portrait video added to Mobile media queries

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,13 +1,35 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faPause, faVolumeMute, faVolumeUp } from '@fortawesome/free-solid-svg-icons';
-import Video from '../../vid/Homepage.mp4';
+import DesktopVideo from '../../vid/Homepage.mp4';
+import MobileVideo from '../../vid/Mobile-Video.mp4';
 import './Video.css';
 
 const Videos = () => {
     const videoRef = useRef(null);
     const [isPlaying, setIsPlaying] = useState(true);
     const [isMuted, setIsMuted] = useState(true);
+    const [videoSource, setVideoSource] = useState(DesktopVideo);
+
+    useEffect(() => {
+        const handleResize = () => {
+            const width = window.innerWidth;
+            if (width <= 475) {
+                setVideoSource(MobileVideo);
+            } else {
+                setVideoSource(DesktopVideo);
+            }
+        };
+
+        // Initial check
+        handleResize();
+
+        // Add event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup event listener on component unmount
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     const handlePlayPause = () => {
         if (videoRef.current.paused) {
@@ -34,7 +56,7 @@ const Videos = () => {
                 loop
                 onClick={handlePlayPause}
             >
-                <source src={Video} type="video/mp4" />
+                <source src={videoSource} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
             <div className="controls">
@@ -47,6 +69,6 @@ const Videos = () => {
             </div>
         </div>
     );
-}
+};
 
 export default Videos;
